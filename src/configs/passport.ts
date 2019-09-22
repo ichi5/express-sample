5eaf48d96997f4badbcd12794210d6b0339d9465
ichi5 <12611729+ichi5@users.noreply.github.com>
2019-09-23 01:54:04 +0900

    Fix serializeUser

--- src/configs/passport.ts
+++ src/configs/passport.ts
@@ -11,23 +11,25 @@ const optionLocalStrategy: IStrategyOptions = {
 };
 
 // ログイン認証
-passport.use(new LocalStrategy(optionLocalStrategy, async (email: string, password: string, done: any) => {
-    const data = await UsersDao.findLoginUser(email, password);
-    if (data === null) {
-        return done(null, false);
-    } else {
-        return done(null, data);
-    }
-}));
+passport.use(
+    new LocalStrategy(optionLocalStrategy, async (email: string, password: string, done: any) => {
+        const data = await UsersDao.findLoginUser(email, password);
+        if (data === null) {
+            return done(null, false);
+        } else {
+            return done(null, data);
+        }
+    }),
+);
 
-// セッションにユーザー情報を格納
+// セッションにユーザーIDを格納
 passport.serializeUser((user: Users, done) => {
-    done(null, user);
+    done(null, user.id);
 });
 
 // セッションに格納されたユーザー情報の検証
-passport.deserializeUser((user: Users, done) => {
-    // TODO: ここでfindOne(user_id)を行いユーザー情報を検証するべき
+passport.deserializeUser(async (id: number, done) => {
+    const user = await UsersDao.findById(id);
     done(null, user);
 });
 
